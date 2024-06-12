fad22dd500bf1888d4f4ff3e2435ead756582a04
hjh3933 <hkh3933@naver.com>
2024-06-12 16:54:59 +0900

    calendar 관련 쿼리 수정

--- src/main/java/project/tikklemoa_back/repository/CalendarRepository.java
+++ src/main/java/project/tikklemoa_back/repository/CalendarRepository.java
@@ -8,14 +8,14 @@ import project.tikklemoa_back.entity.CalendarEntity;
 import java.util.List;
 
 public interface CalendarRepository extends JpaRepository<CalendarEntity, Long> {
-    @Query(value = "SELECT c.date, " +
+    @Query(value = "SELECT DATE(c.date) as date, " +
             "SUM(CASE WHEN c.category = 'MINUS' THEN c.price ELSE 0 END) as totalMinus, " +
             "SUM(CASE WHEN c.category = 'PLUS' THEN c.price ELSE 0 END) as totalPlus, " +
             "(SUM(CASE WHEN c.category = 'MINUS' THEN c.price ELSE 0 END) " +
             "- SUM(CASE WHEN c.category = 'PLUS' THEN c.price ELSE 0 END)) as dateTotal " +
             "FROM calendar c " +
-            "WHERE c.date BETWEEN :startOfMonth AND :endOfMonth AND c.userid = :userid " +
-            "GROUP BY c.date", nativeQuery = true)
+            "WHERE DATE(c.date) BETWEEN :startOfMonth AND :endOfMonth AND c.userid = :userid " +
+            "GROUP BY DATE(c.date)", nativeQuery = true)
     List<Object[]> findByYearAndMonthAndUserIdGroupedByDate(@Param("startOfMonth") String startOfMonth, @Param("endOfMonth") String endOfMonth, @Param("userid") Long userId);
 
     @Query(value = "SELECT * FROM calendar c WHERE c.date BETWEEN :startOfDay AND :endOfDay AND c.userid = :userid", nativeQuery = true)
@@ -27,7 +27,7 @@ public interface CalendarRepository extends JpaRepository<CalendarEntity, Long> 
     @Query(value = "SELECT c.subcategory, " +
             "SUM(c.price) as totalPrice, " +
             "ROUND(SUM(c.price) * 100.0 / " +
-            "(SELECT SUM(c2.price) FROM calendar c2 WHERE c2.category = 'MINUS' AND c2.userid = :userid AND DATE_FORMAT(c2.date, '%Y-%m') = :dateStr), 2) as percentage " +
+            "(SELECT SUM(c2.price) FROM calendar c2 WHERE c2.category = 'MINUS' AND c2.userid = :userid AND c2.date BETWEEN :startOfMonth AND :endOfMonth), 2) as percentage " +
             "FROM calendar c " +
             "WHERE c.category = 'MINUS' AND c.userid = :userid AND c.date BETWEEN :startOfMonth AND :endOfMonth " +
             "GROUP BY c.subcategory", nativeQuery = true)
